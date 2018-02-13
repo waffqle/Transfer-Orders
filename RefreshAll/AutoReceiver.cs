db6c8f24e17f5cc1ceba8f54562337917ef3c496
John Driggers <waffqle@gmail.com>
2018-02-13 09:10:57 -0500

    Add project files.

---
+++ RefreshAll/AutoReceiver.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoReceiver.Properties;
+using Epicor.Mfg.BO;
+using Epicor.Mfg.Core;
+using NLog;
+
+namespace AutoReceiver {
+    public static class AutoReceiver {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main() {
+            try {
+                logger.Info(
+                    $"Connecting to: {Settings.Default.Server}:{Settings.Default.Port} Plant: {Settings.Default.Plant} User: {Settings.Default.User}");
+
+                // Get unreceived packs for consignment plant
+                var consignmentPacks = GetUnreceivedConsignmentPacks();
+
+                /*
+                 * Loop through all the packs!
+                 * Step 1: Make sure destination bins exist for all lines.
+                 * Step 2: Setup the line for receipt.
+                 * Step 3: Add obsolete(source) bins to the deactivation list.
+                 */
+                var oldTrays = new List<Bin>();
+
+                foreach (var consignmentPack in consignmentPacks)
+                    ProcessPack(consignmentPack, oldTrays);
+
+                // Inactivate all the obsolete bins.
+                DeactiveBins(oldTrays);
+            } catch (Exception ex) {
+                logger.Error($"Something went seriously wrong. Processing cancelled. Error: {ex.Message}");
+            } finally {
+                ShutItDown();
+                logger.Info("All Done!");
+            }
+        }
+
+        private static void ShutItDown() {
+            session.Dispose();
+        }
+
+        #region Pack Processing
+        private static IEnumerable<TFShipHeadListDataSet.TFShipHeadListRow> GetUnreceivedConsignmentPacks() {
+            logger.Info("Retrieving pending packs...");
+
+            var packs = transOrderReceipt.GetListBasicSearch(false, 0, 1, out _);
+
+            var consignmentPacks = packs.TFShipHeadList.Cast<TFShipHeadListDataSet.TFShipHeadListRow>()
+                                        .Where(t => string.Equals(t.ToPlant, "CONSIGN", StringComparison.InvariantCultureIgnoreCase)).ToList();
+            logger.Info($"Found {consignmentPacks.Count} packs.");
+
+            return consignmentPacks;
+        }
+
+        /// <summary>
+        ///     Create receipt bins, receive lines, update list of bins to deactivate.
+        /// </summary>
+        private static void ProcessPack(TFShipHeadListDataSet.TFShipHeadListRow consignmentPack, List<Bin> traysToInactivate) {
+            try {
+                logger.Info($"Processing pack {consignmentPack.PackNum}...");
+                logger.Info($"Fetching order {consignmentPack.ShortChar01}...");
+
+                var order         = transferOrderEntry.GetByID(consignmentPack.ShortChar01);
+                var warehouseCode = order.TFOrdHed[0].ShortChar10;
+
+                // We only want NeedBy dates before today
+                var needBy = order.TFOrdHed[0].Date01;
+
+                if (needBy > DateTime.Today)
+                    return;
+
+                logger.Info($"Fetching receipt {consignmentPack.PackNum}...");
+                var receipt = transOrderReceipt.GetByID(consignmentPack.PackNum);
+
+                logger.Info($"Found {receipt.PlantTran.Count} receipt lines...");
+
+                foreach (var plantTran in receipt.PlantTran.Cast<TransOrderReceiptDataSet.PlantTranRow>())
+                    ProcessLine(traysToInactivate, order, plantTran, warehouseCode, receipt);
+
+                logger.Info("Pack complete!");
+            } catch (Exception ex) {
+                logger.Error($"Processing failed. Error: {ex.Message}");
+            }
+        }
+
+        private static void ProcessLine(List<Bin> traysToInactivate, TransferOrderEntryDataSet order, TransOrderReceiptDataSet.PlantTranRow line,
+                                        string warehouseCode, TransOrderReceiptDataSet receipt) {
+            try {
+                // Get the order line that matches this transaction to retrieve replenishment info.
+                var ordLineDS = order.TFOrdDtl.Cast<TransferOrderEntryDataSet.TFOrdDtlRow>()
+                                     .FirstOrDefault(o => o.TFOrdNum==line.TFOrdNum && o.TFLineNum==line.TFLineNum);
+
+                if (ordLineDS==null)
+                    throw new Exception("Unable to locate matching order line.");
+
+                var trayKey          = ordLineDS.Character10;
+                var replenishmentBin = ordLineDS.ShortChar04;
+
+                logger.Info($"Line: {line.TFLineNum}. TrayKey: {trayKey} Replenishment bin: {replenishmentBin}");
+
+                /*
+                 * Set the receiving bin!
+                 * If someone specified a 'trayKey' and replenishment bin, use that.
+                 * Otherwise, ensure a duplicate of the source bin exists in the destination DB and use that.
+                 */
+                if (string.IsNullOrEmpty(trayKey))
+                    line.ReceiveToBinNum = !string.IsNullOrEmpty(replenishmentBin) ? replenishmentBin : "1";
+                else
+                    CheckBinConfiguration(line, order, trayKey, replenishmentBin, traysToInactivate);
+
+                // Update line!
+                line.ReceiveToWhseCode = warehouseCode;
+                line.RecTranDate       = DateTime.Now;
+                line.PlntTranReference = $"Consign Order: {line.TFOrdNum}";
+
+                transOrderReceipt.PreUpdate(receipt, out _);
+                transOrderReceipt.Update(receipt);
+            } catch (Exception ex) {
+                logger.Error($"Error on line {line.TFLineNum}. Error: {ex.Message}");
+            }
+        }
+        #endregion
+
+        #region Bin Processing
+        /// <summary>
+        ///     Ensure a bin exists in the destination warehouse with the same ID as the source bin.
+        ///     Update deactivation list.
+        /// </summary>
+        private static void CheckBinConfiguration(TransOrderReceiptDataSet.PlantTranRow plantTran, TransferOrderEntryDataSet order, string trayKey,
+                                                  string replenishmentBin, List<Bin> traysToInactivate) {
+            try {
+                var toWarehouse           = order.TFOrdHed[0].ShortChar10;
+                var toWarehouseZone       = order.TFOrdHed[0].ShortChar02;
+                var fromWarehouse         = plantTran.FromWarehouseCode;
+                var binNum                = plantTran.BinNum;
+                var packNum               = plantTran.PackNum.ToString();
+                var usingReplenishmentBin = true;
+
+                // If trayKey is specified, we use the replenishment bin or fall back to "1".
+                if (string.IsNullOrEmpty(trayKey)) {
+                    binNum                = !string.IsNullOrEmpty(replenishmentBin) ? replenishmentBin : "1";
+                    usingReplenishmentBin = false;
+                }
+
+                logger.Info($"Validating bins. Going from {fromWarehouse}/{binNum} to {toWarehouse}/{binNum}.");
+
+                // Look up the source bin so we can copy properties to destination.
+                var fromBinDS = whseBin.GetRows($@"WarehouseCode = '{fromWarehouse}' and BinNum = '{binNum}'", "", 0, 1, out _);
+
+                UpsertBin(toWarehouse, binNum, toWarehouseZone, packNum, usingReplenishmentBin, fromBinDS);
+
+                // Make sure the fromBin is in our deactivation list. (Unless it's a replenishment bin.)
+                // It's going to be empty and obsolete after receipt.
+                if (!usingReplenishmentBin && !traysToInactivate.Any(t => t.BinNum==binNum && t.WarehouseCode==fromWarehouse))
+                    traysToInactivate.Add(new Bin {
+                        BinNum        = binNum,
+                        WarehouseCode = fromWarehouse
+                    });
+
+                plantTran.ReceiveToBinNum = binNum;
+            } catch (Exception ex) {
+                logger.Error($"Failed to validate bin configuration. Error: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        ///     Make sure bin exists with correct properties
+        /// </summary>
+        private static void UpsertBin(string warehouse, string binNum, string zoneID, string packNum, bool usingReplenishmentBin,
+                                      WhseBinDataSet fromBinDS) {
+            try {
+                logger.Info($"Upserting {warehouse}/{binNum}...");
+
+                // Maybe our bin already exists?
+                var toBinDS = whseBin.GetRows($@"WarehouseCode = '{warehouse}' and BinNum = '{binNum}'", "", 0, 1, out _);
+
+                // Find it? Create it? Just make sure it exists!
+                if (toBinDS.WhseBin.Count > 0) {
+                    logger.Info("Found bin!");
+                } else {
+                    logger.Info("Creating bin...");
+                    toBinDS = new WhseBinDataSet();
+                    whseBin.GetNewWhseBin(toBinDS, warehouse);
+                    toBinDS.WhseBin[0].BinNum = binNum;
+                    toBinDS.WhseBin[0].Description = binNum;
+                }
+
+                // Get those fields in sync!
+                // Don't change the properties on a bin that may have already had them specified.
+                if (!usingReplenishmentBin) {
+                    logger.Info("Syncing bin fields...");
+                    toBinDS.WhseBin[0].Description = fromBinDS.WhseBin[0].Description;
+                    toBinDS.WhseBin[0].ZoneID      = zoneID;
+                    toBinDS.WhseBin[0].Character04 = packNum;
+                    toBinDS.WhseBin[0].Character01 = fromBinDS.WhseBin[0].Character01; // Tray Type
+                    toBinDS.WhseBin[0].Character06 = fromBinDS.WhseBin[0].Character06; // Tray Revision
+                    toBinDS.WhseBin[0].NonNettable = false;
+                }
+
+                // The bin has to be active or we can't receieve into it.
+                toBinDS.WhseBin[0].InActive = false;
+
+                whseBin.Update(toBinDS);
+            } catch (Exception ex) {
+                logger.Error($"Error upserting bin. Error: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        ///     Go through a list of bins and deactivate them all.
+        ///     Bins with stuff in them will be ignored.
+        /// </summary>
+        private static void DeactiveBins(List<Bin> bins) {
+            logger.Info($"Deactivating {bins.Count} bins...");
+
+            foreach (var bin in bins)
+                DeactivateBin(bin);
+
+            logger.Info("Done with bins!");
+        }
+
+        private static void DeactivateBin(Bin bin) {
+            try {
+                logger.Info($"Deactivating bin {bin}...");
+
+                // Is this a valid bin?
+                partBinSearch.CheckBin(bin.WarehouseCode, bin.BinNum, out var errMsg);
+
+                // Something went wrong!
+                if (!string.IsNullOrEmpty(errMsg))
+                    throw new Exception(errMsg);
+
+                // Is there anything in the bin?
+                if (partBinSearch.GetBinContents(bin.WarehouseCode, bin.BinNum).PartBinSearch.Count > 0) {
+                    logger.Info("Bin isn't empty. Deactivation cancelled.");
+                    return;
+                }
+
+                // Inactivate our bin!
+                var wbDS_InActive = whseBin.GetByID(bin.WarehouseCode, bin.BinNum);
+                wbDS_InActive.WhseBin[0].InActive = true;
+                whseBin.Update(wbDS_InActive);
+            } catch (Exception ex) {
+                logger.Error($"Failed to deactivate bin. Error: {ex.Message}");
+            }
+        }
+        #endregion
+
+        #region Epicor Properties
+        private static Session _session;
+        private static Session session =>
+            _session ??
+            (_session = new Session(Settings.Default.User, Settings.Default.Pass, $"AppServerDC://{Settings.Default.Server}:{Settings.Default.Port}",
+                                    Session.LicenseType.Default) {
+                    PlantID = Settings.Default.Plant
+                });
+        private static WhseBin            whseBin            => new WhseBin(session.ConnectionPool);
+        private static PartBinSearch      partBinSearch      => new PartBinSearch(session.ConnectionPool);
+        private static TransOrderReceipt  transOrderReceipt  => new TransOrderReceipt(session.ConnectionPool);
+        private static TransferOrderEntry transferOrderEntry => new TransferOrderEntry(session.ConnectionPool);
+        #endregion
+    }
+
+    internal class Bin {
+        public string BinNum;
+        public string WarehouseCode;
+    }
+}
---
+++ RefreshAll/RefreshAllShipments (2).cs
@@ -0,0 +1,232 @@
+﻿using Epicor.Mfg.BO;
+using Epicor.Mfg.Core;
+using Microsoft.Office.Interop.Outlook;
+using OutlookApp = Microsoft.Office.Interop.Outlook.Application;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefreshAll
+{
+    public class RefreshAllShipments
+    {
+        //main method
+        static void Main()
+        {
+            //a tray catch block
+            //try
+            //{
+                //make sure we are just accessing a session for code that follows
+                using (Session epiSession = new Session("consignment", "consignment", "AppServerDC://app-server-1:9401", Session.LicenseType.Default))
+                {
+                    //set the plant to be the consignment one
+                    epiSession.PlantID = "CONSIGN";
+
+                    //connect to a Trans Order Receipt BO
+                    TransOrderReceipt tor = new TransOrderReceipt(epiSession.ConnectionPool);
+
+                    //connect to a Transfer Order Entry BO
+                    TransferOrderEntry toe = new TransferOrderEntry(epiSession.ConnectionPool);
+
+                    //call the warehouse bin bo
+                    WhseBin wb = new WhseBin(epiSession.ConnectionPool);
+
+                    //connect to a part bin search bo
+                    PartBinSearch pbs = new PartBinSearch(epiSession.ConnectionPool);
+
+                    //a list object for keeping track of which trays need to be inactivated
+                    ArrayList WhseBinList = new ArrayList();
+
+                    //a string that contains the from warehouse code
+                    string FromWhseCode = "";
+
+                    //get a dataset of the packs that aren't received
+                    bool morePages;
+                    TFShipHeadListDataSet listDS = tor.GetListBasicSearch(false, 0, 1, out morePages);
+
+                    //loop through the packs that haven't been received
+                    for (int i = 0; i < listDS.Tables["TFShipHeadList"].Rows.Count; i++)
+                    {
+                        //make sure this is a consignment order, and not a normal transfer
+                        if (String.Compare(listDS.Tables["TFShipHeadList"].Rows[i]["ToPlant"].ToString(), "CONSIGN") == 0)
+                        {
+                            //get the order information
+                            TransferOrderEntryDataSet toeDS = toe.GetByID(listDS.Tables["TFShipHeadList"].Rows[i]["ShortChar01"].ToString());
+
+                            //check to see if the need by date is today or later
+                            if (DateTime.Compare(Convert.ToDateTime(toeDS.Tables["TFOrdHed"].Rows[0]["Date01"]), DateTime.Today) == 0 ||
+                                DateTime.Compare(Convert.ToDateTime(toeDS.Tables["TFOrdHed"].Rows[0]["Date01"]), DateTime.Today) < 0)
+                            {
+                                //get the data from the current pack
+                                TransOrderReceiptDataSet torDS = tor.GetByID(Convert.ToInt32(listDS.Tables["TFShipHeadList"].Rows[i]["PackNum"]));
+
+                                //set the from warehouse code string
+                                FromWhseCode = torDS.Tables["PlantTran"].Rows[0]["FromWarehouseCode"].ToString();
+
+                                //loop through all of the lines on the pack
+                                for (int j = 0; j < torDS.Tables["PlantTran"].Rows.Count; j++)
+                                {
+                                    //get information about the order line
+                                    bool morePages2;
+                                    TransferOrderEntryDataSet ordLineDS = toe.GetRows("TFOrdNum = '" + torDS.Tables["TFShipDtl"].Rows[j]["TFOrdNum"] + "'", "", "TFOrdLine = '" + torDS.Tables["TFShipDtl"].Rows[j]["TFOrdLine"] + "'", 0, 1, out morePages2);
+
+                                    //check to see if there is not a tray key
+                                    if (String.IsNullOrEmpty(ordLineDS.Tables["TFOrdDtl"].Rows[0]["Character10"].ToString()))
+                                    {
+                                        //check to see if there is a replenish to bin selected, if not, move to bin 1
+                                        if (!String.IsNullOrEmpty(ordLineDS.Tables["TFOrdDtl"].Rows[0]["ShortChar04"].ToString()))
+                                        {
+                                            //set the bin
+                                            torDS.Tables["PlantTran"].Rows[j]["ReceiveToBinNum"] = ordLineDS.Tables["TFOrdDtl"].Rows[0]["ShortChar04"]; //the replenish to bin number
+                                        }
+                                        else
+                                        {
+                                            //set the bin to 1
+                                            torDS.Tables["PlantTran"].Rows[j]["ReceiveToBinNum"] = 1;
+                                        }
+                                    }
+                                    //if there is a tray key
+                                    else
+                                    {
+                                        //get the information about the from warehouse bin
+                                        bool morePages3;
+                                        WhseBinDataSet wbDS_FromBin = wb.GetRows("WarehouseCode = '" + torDS.Tables["PlantTran"].Rows[j]["FromWarehouseCode"] + "' and BinNum = '" + torDS.Tables["PlantTran"].Rows[j]["BinNum"] + "'", "", 0, 1, out morePages3);
+
+                                        //use the to warehouse and from bin to determine if it exists in the distributor plant
+                                        bool morePages4;
+                                        WhseBinDataSet wbDS_ToBin = wb.GetRows("WarehouseCode = '" + toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar10"] + "' and BinNum = '" + torDS.Tables["PlantTran"].Rows[j]["BinNum"] + "'", "", 0, 1, out morePages4);
+
+                                        //if there isn't a row, add a new bin to the warehouse
+                                        if (wbDS_ToBin.Tables["WhseBin"].Rows.Count <= 0)
+                                        {
+                                            //get a new whsebin data set
+                                            WhseBinDataSet wbDS_new = new WhseBinDataSet();
+
+                                            //create a new bin
+                                            wb.GetNewWhseBin(wbDS_new, toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar10"].ToString());
+
+                                            //set the bin number, bin description, warehouse zone, tray type, and tray revision
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["BinNum"] = wbDS_FromBin.Tables["WhseBin"].Rows[0]["BinNum"];
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["Description"] = wbDS_FromBin.Tables["WhseBin"].Rows[0]["Description"];
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["ZoneID"] = toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar02"]; //whse zone from the order
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["Character01"] = wbDS_FromBin.Tables["WhseBin"].Rows[0]["Character01"];
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["Character06"] = wbDS_FromBin.Tables["WhseBin"].Rows[0]["Character06"];
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["Character04"] = torDS.Tables["PlantTran"].Rows[j]["PackNum"];
+
+                                            //set the bin to be non-nettable 
+                                            wbDS_new.Tables["WhseBin"].Rows[0]["NonNettable"] = false;
+
+                                            //update the information
+                                            wb.Update(wbDS_new);
+                                        }
+                                        //if there is a row, update the warehouse zone and last pack number
+                                        else
+                                        {
+                                            wbDS_ToBin.Tables["WhseBin"].Rows[0]["ZoneID"] = toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar02"];
+                                            wbDS_ToBin.Tables["WhseBin"].Rows[0]["Character04"] = torDS.Tables["PlantTran"].Rows[j]["PackNum"];
+
+                                            //update the information 
+                                            wb.Update(wbDS_ToBin);
+                                        }
+
+                                        //check to see if the current plant tran row's bin isn't in the list
+                                        if (!WhseBinList.Contains(wbDS_FromBin.Tables["WhseBin"].Rows[0]["BinNum"].ToString()))
+                                        {
+                                            //add the bin number to the list
+                                            WhseBinList.Add(wbDS_FromBin.Tables["WhseBin"].Rows[0]["BinNum"].ToString());
+                                        }
+
+                                        //re-retrieve the data
+                                        wbDS_ToBin = wb.GetRows("WarehouseCode = '" + toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar10"] + "' and BinNum = '" + torDS.Tables["PlantTran"].Rows[j]["BinNum"] + "'", "", 0, 1, out morePages4);
+
+                                        //check to see if the to warehouse bin is inactive
+                                        if ((bool)wbDS_ToBin.Tables["WhseBin"].Rows[0]["InActive"] == true)
+                                        {
+                                            //activate it
+                                            wbDS_ToBin.Tables["WhseBin"].Rows[0]["InActive"] = false;
+
+                                            //update
+                                            wb.Update(wbDS_ToBin);
+                                        }
+
+                                        //update the from bin's pack number
+                                        wbDS_FromBin.Tables["WhseBin"].Rows[0]["Character04"] = torDS.Tables["PlantTran"].Rows[j]["PackNum"];
+                                        wb.Update(wbDS_FromBin);
+
+                                        //set the to warehouse code
+                                        torDS.Tables["PlantTran"].Rows[j]["ReceiveToBinNum"] = wbDS_FromBin.Tables["WhseBin"].Rows[0]["BinNum"];
+                                    }
+
+                                    //change the receive to warehouse
+                                    torDS.Tables["PlantTran"].Rows[j]["ReceiveToWhseCode"] = toeDS.Tables["TFOrdHed"].Rows[0]["ShortChar10"]; //the warehouse code
+
+                                    //set the receive trans date to be right now
+                                    torDS.Tables["PlantTran"].Rows[j]["RecTranDate"] = DateTime.Now;
+
+                                    //update the reference to display the consignment order number
+                                    torDS.Tables["PlantTran"].Rows[j]["PlntTranReference"] = "Consign Order: " + torDS.Tables["PlantTran"].Rows[j]["TFOrdNum"];
+
+                                    //call the pre-update method
+                                    bool requiresUserInput;
+                                    tor.PreUpdate(torDS, out requiresUserInput);
+
+                                    //call the update method
+                                    tor.Update(torDS);
+                                }
+                            }
+                        }
+                    }
+
+                    //check to see if there are any bins in the whse bin list
+                    if (WhseBinList.Count > 0)
+                    {
+                        //loop through the bins in the whse bin list and inactivate them
+                        for (int z = 0; z < WhseBinList.Count; z++)
+                        {
+                            //check to see if this is a valid whse bin
+                            string errMsg;
+                            pbs.CheckBin(FromWhseCode, WhseBinList[z].ToString(), out errMsg);
+
+                            //if there isn't an error message
+                            if (String.IsNullOrEmpty(errMsg))
+                            {
+                                //check to see if there are still contents in the bin
+                                PartBinSearchDataSet pbsDS = pbs.GetBinContents(FromWhseCode, WhseBinList[z].ToString());
+
+                                //if there aren't any rows
+                                if (pbsDS.Tables["PartBinSearch"].Rows.Count <= 0)
+                                {
+                                    //get the dataset for each whse bin that needs to be inactivated
+                                    WhseBinDataSet wbDS_InActive = wb.GetByID(FromWhseCode, WhseBinList[z].ToString());
+
+                                    //set the inactive checkbox
+                                    wbDS_InActive.Tables["WhseBin"].Rows[0]["InActive"] = true;
+
+                                    //update 
+                                    wb.Update(wbDS_InActive);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            /*catch
+            {
+                //create a new outlook application
+                OutlookApp oApp = new OutlookApp();
+                //create a new mail item
+                MailItem mItem = oApp.CreateItem(OlItemType.olMailItem);
+                //set the fields for the email
+                mItem.To = "gkoutrelakos@amendia.com; solson@amendia.com";
+                mItem.Subject = "Receive All Error";
+                mItem.HTMLBody = "<html><body>There was an error while trying to run the Receive All script.</body></html>";
+                //set the importance
+                mItem.Importance = OlImportance.olImportanceHigh;
+                //send the email
+                ((_MailItem)mItem).Send();
+            }*/
+        }
+    }
+//}
